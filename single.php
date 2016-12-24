317be66012b37eba086b64aa3edf2c5e5a4d2b08
matheusgimenez <contato@matheusgimenez.com.br>
2016-12-24 14:04:33 -0200

    adicionando blog

---
+++ single.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package OnePress
+ */
+
+get_header(); ?>
+
+	<div id="content" class="site-content">
+		<?php if(has_post_thumbnail() && $img = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full', true )):?>
+		<div class="page-fullheader">
+			<img src="<?php echo $img[0];?>"/>
+		</div>
+		<?php endif;?>
+		<div class="page-header">
+	<div class="container">
+		<header class="entry-header">
+			<div class="col-sm-9">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				<div class="entry-meta">
+					<?php onepress_posted_on(); ?>
+				</div><!-- .entry-meta -->
+			</div>
+		</header><!-- .entry-header -->
+	</div>
+		</div>
+		<div id="content-inside" class="container right-sidebar">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+					<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+					?>
+
+				<?php endwhile; // End of the loop. ?>
+
+
+		</div><!--#content-inside -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
